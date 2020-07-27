a723f587d953fbd6e8c3a02928fa60dd3533de77
timeoutguy <marcus.dev@protonmail.com>
2020-07-27 09:49:53 -0300

    screens

---
+++ src/screens/Browse/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Browse = () => {
+  return <View />;
+}
+
+export default Browse;
---
+++ src/screens/Following/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Following = () => {
+  return <View />;
+}
+
+export default Following;
